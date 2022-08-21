ab4692c3ff170d50cbbc364339a382c569c346d0
ShubhamMohanty680 <101620532+ShubhamMohanty680@users.noreply.github.com>
2022-08-21 10:38:05 +0530

    update a file

---
+++ tut2.js
@@ -0,0 +1,12 @@
+console.time('Your Code Took');
+console.log('Hello Console');
+console.log(4+34);
+console.log(true);
+console.log([34,2,1,2]);
+console.log({harry:"this",marks:34});  
+console.table({harry:"this",marks:34});  
+console.warn("This is warning");
+// console.clear();
+console.timeEnd('Your Code Took');
+// console.assert(500<189,'This is not possible');
+console.error('This is an error')
---
+++ tut3.js
@@ -0,0 +1,31 @@
+console.log("tut3");
+// Variabels in js
+// var,let,const
+  var name="Shubham";
+  var channel;
+  channel="CodeWithHarry";
+  console.log(name,channel);
+  const name1="Mohanty";
+  var city="Delhi";
+//name1="njx"; const variables cannot be changed
+  console.log(name1);
+
+  //let variable is used for a particular block of code
+{
+    let city="Mumbai";
+    console.log(city);
+}
+console.log(city);
+const arr=[4,465,7.6,79,4.6];
+// arr=[90,49,59]  Not possible in case of constant array
+arr.push(100);
+console.log(arr);
+
+/*  Most common programming case types:
+
+   1. camelCase
+   2. kebab-case
+   3. snake_case
+   4. PascalCase
+
+*/
